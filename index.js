b87403ba6c54483646702de5f81edaedcf6e3635
shanker919 <91017250+shanker919@users.noreply.github.com>
2021-09-30 19:58:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var imgObj = null;
+var animate;
+function init(){
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position ='relative';
+    imgObj.style.left = '0px';
+}
+function moveRight(){
+    imgObj.style.left = parseInt(imgObj.style.left) +10 +'px';
+    animate = setTimeout(moveRight,20);//call moveRight in 20msec
+}
+function stop(){
+    clearTimeout(animate);
+    imgObj.style.left ='0px';
+}
+window.onload = init;
